86f15e251fa3e002838d9be315b3fcb0dc395ecb
Eduardo García Zambrana <edugz19@outlook.es>
2021-05-16 13:48:21 +0200

    Corrección consultas

--- Fase2/ConsultasIberos.sql
+++ Fase2/ConsultasIberos.sql
@@ -1,34 +1,34 @@
---1- Muestra los nombres de los guerros.
+-- 1. Muestra los nombres de los guerros.
 SELECT nombre FROM guerreros;
 
---2- Muestra los nombres de los dioses.
+-- 2. Muestra los nombres de los dioses.
 SELECT nombre_dios FROM dioses_guerreros;
 
---3- Muestra la descripción de los diozes
+-- 3. Muestra la descripción de los diozes
 SELECT descripcion FROM dioses;
 
---4- Muestra los codigos de las organizaciones militares de la caballería.
+-- 4. Muestra los codigos de las organizaciones militares de la caballería.
 SELECT codigo_org_militar FROM caballeria
 
---5- Muestra el id asignado al lider y el nombre del lider.
+-- 5. Muestra el id asignado al lider y el nombre del lider.
 SELECT id_lider, nombre FROM lider;
 
 
---WHERE y JOIN
+-- WHERE y JOIN
 
---1- Muestra el nombre del dios y de las armas donde el id de guerrero sea igual a 1.
-SELECT nombre_dios, a.nombre
-FROM dioses_guerreros,armas a
-WHERE id_guerrero = 1;
+-- 1. Muestra el nombre del arma, y nombre y primer apellido de todos los guerreros.
+SELECT a.nombre, concat_ws(" ", g.nombre, g.apellido1) "Nombre guerrero"
+FROM guerreros g, armas a
+WHERE g.id = a.id_guerrero;
 
---2-Muestra el nombre del asentamiento y el nombre del guerrero .
-SELECT a.nombre_asentamiento,g.nombre 
-FROM asentamiento a
-INNER JOIN guerreros g;
+-- 2. Muestra la localización y el nombre de todos los guerreros que pertenece a ella.
+SELECT a.localizacion, concat_ws(" ", g.nombre, g.apellido1) "Nombre guerrero"
+FROM asentamiento a INNER JOIN guerreros g 
+ON a.nombre = g.nombre_asentamiento;
 
---SUBCONSULTA
+-- SUBCONSULTA
 
---1- Nombre de arma de los guerreros que pertenecen a Augusta Emerita
+-- 1. Nombre de arma de los guerreros que pertenecen a Augusta Emerita
 select nombre 
 from armas 
 where id_guerrero = (select id 
